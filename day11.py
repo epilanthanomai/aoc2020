71df1f16e95dcaa8f08de996061b574146aa0f33
Hope Ranker <hope.ranker@gmail.com>
2020-12-11 12:07:25 -0500

    day 11 problem 2

--- day11.py
+++ day11.py
@@ -1,12 +1,58 @@
 #!/usr/bin/env python3
 
+import sys
+from functools import partial
 
-def main():
+
+def main(neighbor_strategy, neighbor_tolerance):
     with open("data/day11-ferryseats.txt") as seat_file:
         seats = list(line.strip() for line in seat_file)
-    stable = stabilize(step_seats, seats)
+    neighbors = calculate_neighbors(seats, neighbor_strategy)
+    step = partial(step_seats, neighbors, neighbor_tolerance)
+    stable = stabilize(step, seats)
     occupied = "".join(stable).count("#")
     print(occupied)
+
+
+def calculate_neighbors(seats, strategy):
+    return [
+        [strategy(seats, i, j) for j in range(len(seats[i]))] for i in range(len(seats))
+    ]
+
+
+DIRECTIONS = (
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+)
+
+
+def adjacent_seats(seats, i, j):
+    return [
+        (i + ni, j + nj)
+        for (ni, nj) in DIRECTIONS
+        if 0 <= i + ni < len(seats) and 0 <= j + nj < len(seats[i])
+    ]
+
+
+def visible_seats(seats, i, j):
+    raw_result = [
+        visible_seat_in_direction(seats, i, j, ni, nj) for (ni, nj) in DIRECTIONS
+    ]
+    return [seat for seat in raw_result if seat is not None]
+
+
+def visible_seat_in_direction(seats, i, j, si, sj):
+    ni, nj = si, sj
+    while 0 <= i + ni < len(seats) and 0 <= j + nj < len(seats[i]):
+        if seats[i + ni][j + nj] != ".":
+            return i + ni, j + nj
+        ni, nj = ni + si, nj + sj
 
 
 def stabilize(f, start):
@@ -23,44 +69,34 @@ def iteratively_apply(f, state):
         yield state
 
 
-def step_seats(state):
+def step_seats(neighbors, neighbor_tolerance, state):
     return [
         "".join(
-            next_for_seat(seat, get_neighbors(state, i, j))
+            next_for_seat(
+                neighbor_tolerance, seat, get_neighbor_state(neighbors, state, i, j)
+            )
             for (j, seat) in enumerate(row)
         )
         for (i, row) in enumerate(state)
     ]
 
 
-def next_for_seat(seat, neighbors):
+def next_for_seat(neighbor_tolerance, seat, neighbors):
     if seat == "L":
         return "#" if neighbors.count("#") == 0 else "L"
     elif seat == "#":
-        return "L" if neighbors.count("#") >= 4 else "#"
+        return "L" if neighbors.count("#") >= neighbor_tolerance else "#"
     elif seat == ".":
         return "."
 
 
-NEIGHBOR_CANDIDATES = (
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-)
-
-
-def get_neighbors(state, i, j):
-    return "".join(
-        state[i + ni][j + nj]
-        for (ni, nj) in NEIGHBOR_CANDIDATES
-        if 0 <= i + ni < len(state) and 0 <= j + nj < len(state[i + ni])
-    )
+def get_neighbor_state(neighbors, state, i, j):
+    return "".join(state[ni][nj] for (ni, nj) in neighbors[i][j])
 
 
 if __name__ == "__main__":
-    main()
+    neighbor_strategy, neighbor_tolerance = {
+        "near": (adjacent_seats, 4),
+        "far": (visible_seats, 5),
+    }[sys.argv[1]]
+    main(neighbor_strategy, neighbor_tolerance)
--- test_answers.sh
+++ test_answers.sh
@@ -45,7 +45,8 @@ check "./day09.py 0" 15353384
 check "./day09.py 1" 2466556
 check "./day10.py" 2312
 check "./day10.py paths" 12089663946752
-check "./day11.py" 2251
+check "./day11.py near" 2251
+check "./day11.py far" 2019
 
 echo totals
 echo checks $checks
